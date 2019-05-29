7b22086340fb5af28bcf101115e44147b10c4039
Gao <you@example.com>
2019-05-29 19:38:53 -0400

    Basically done

---
+++ scrolling-platform-game/spring.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class spring here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class spring extends Platform
+{
+    /**
+     * Constructor
+     * 
+     * Called once when object is created.
+     */
+    spring(int scrollableWorldX, int scrollableWorldY)
+    {
+        super(scrollableWorldX, scrollableWorldY);
+    }
+    
+    /**
+     * Act - do whatever the spring wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
